c29d4dac09c17d31d9f6987bba3b56f5346ab40e
Bangtu21 <bangtu2005.com@gmail.com>
2024-11-27 02:14:02 +0700

    Add files via upload

---
+++ Session6ex01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int num, sum = 0;
+    for (int i = 0; i < 5; i++) {
+        printf("Nhap so thu %d: ", i + 1);
+        scanf("%d", &num);
+        if (num % 2 != 0) {
+            sum = sum + num;
+        }
+    }
+    printf("Tong cac so le là: %d\n", sum);
+}
+
